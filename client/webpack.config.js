004a884b498ab11f7dfd39474f985606242e0851
LatinLady <marielblyden@hotmail.com>
2023-06-05 22:24:36 -0400

    modified files and updating

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
